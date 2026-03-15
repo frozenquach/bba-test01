478b1f1a55709f8846b70a5ee24181cdfd9b48f9
test 01 - Duc <frozenquach@gmail.com>
2026-03-15 16:12:30 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,7 @@
+let playerName="Mario"
+let currentLives=3
+const Level1= 25
+const Level2= 30
+const Level3= 45
+let result=(Level1+Level2+Level3)%3
+console.log(`Coin dư sau khi chia hết cho 3: ${result}`)
